31325ef2e197172f25e4704480f2b06c531489d4
Havan <havanagrawal@gmail.com>
2016-02-14 12:09:45 +0530

    Add more arithmetic to ColumnVector

--- src/main/matrix/ColumnVector.scala
+++ src/main/matrix/ColumnVector.scala
@@ -8,35 +8,55 @@ package matrix
  * TODO: Make this class indexable
  */
 class ColumnVector(val vec: Seq[Double]) {
-  val length = vec.size
-
+  lazy val length = vec.size
+  lazy val magnitude = math.sqrt(this.map(x => x * x).vec.sum)
+  
   def +(that: ColumnVector) = {
     new ColumnVector(this.vec.zip(that.vec).map {
       case (x, y) => x + y
     })
   }
-  
+
   def -(that: ColumnVector) = {
-    this + that.map(x => -x)
+    this + (-that)
   }
-  
+
   def map(f: => (Double => Double)) = {
     new ColumnVector(this.vec.map(f))
   }
-  
+
   def foreach(f: => Double => Unit) = {
     this.vec.foreach(f)
   }
-  
+
+  /**
+   * Multiplies the two ColumnVectors element by element,
+   * adds the results,
+   * and returns a scalar value
+   */
+  def dotProduct(that: ColumnVector) = {
+    this.vec.zip(that.vec).map {
+      case (x, y) => x * y
+    }.sum
+  }
+
+  def *(scalar: Double) = {
+    this.map(x => x * scalar)
+  }
+
+  def unary_-() = {
+    this.map(x => -x)
+  }
+
   // TODO: Return a matrix
   def zip(that: ColumnVector) = {
-    throw new UnsupportedOperationException("This is yet to be implemented. Sorry!") 
+    throw new UnsupportedOperationException("This is yet to be implemented. Sorry!")
   }
-  
+
   def ==(that: ColumnVector) = {
     this.vec == that.vec
   }
-  
+
   override def toString() = {
     vec.toString()
   }
@@ -46,7 +66,7 @@ object ColumnVector {
   def unitVector(length: Int) = {
     new ColumnVector(Seq.fill(length)(1))
   }
-  
+
   def zeroVector(length: Int) = {
     new ColumnVector(Seq.fill(length)(0))
   }
--- src/test/matrix/ColumnVectorTest.scala
+++ src/test/matrix/ColumnVectorTest.scala
@@ -6,24 +6,48 @@ package matrix
 
 import org.scalatest.FlatSpec
 
-class ColumnVectorTest extends FlatSpec{
+class ColumnVectorTest extends FlatSpec {
+
+  import ColumnVector._
+
+  "A unit vector" should "have only 1's" in {
+    val unit = unitVector(10)
+    unit.foreach(value => assert(value == 1))
+  }
+
+  "A zero vector" should "have only 0's" in {
+    val zero = zeroVector(10)
+    zero.foreach(value => assert(value == 0))
+  }
+  
   "A ColumnVector" should "be printable" in {
     val columnVec = new ColumnVector(List(1, 2, 10, 3))
     println(columnVec)
   }
-  
-  "A unit vector" should "have only 1's" in {
-    val unit = ColumnVector.unitVector(10)
-    unit.foreach(value => assert(value == 1))
-  }
-  
-  "A zero vector" should "have only 0's" in {
-    val zero = ColumnVector.zeroVector(10)
-    zero.foreach(value => assert(value == 0))
-  }
-  
-  "A ColumnVector" should "remain the same when added to zero vector" in {
+
+  it should "remain the same when added to zero vector" in {
     val columnVec = new ColumnVector(List(1, 2, 10, 3))
-    assert(columnVec == (columnVec + ColumnVector.zeroVector(columnVec.length)))
-  } 
+    assert(columnVec == (columnVec + zeroVector(columnVec.length)))
+  }
+
+  it should "remain the same when multiplied by 1" in {
+    val columnVec = new ColumnVector(List(1, 2, 10, 3))
+    assert(columnVec == columnVec * 1)
+  }
+  
+  it should "result in zero when multiplied with a zero vector" in {
+    val columnVec = new ColumnVector(List(1, 2, 10, 3))
+    val len = columnVec.length
+    val zero = zeroVector(len)
+    assert(0 == (columnVec.dotProduct(zero)))
+  }
+
+  it should "result in a zero vector when multiplied with 0" in {
+    val columnVec = new ColumnVector(List(1, 2, 10, 3))
+    val len = columnVec.length
+    val zero = zeroVector(len)
+    assert(zero == columnVec * 0)
+  }
+
+  
 }
